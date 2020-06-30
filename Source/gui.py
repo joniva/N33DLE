845ad886cc1459e80d6235c62676d27e1d8f6b20
Joniva <jonathanivanov@gmail.com>
2020-06-29 20:09:41 -0700

    Changes

--- Source/gui.py
+++ Source/gui.py
@@ -1,1 +1,2 @@
 #This file will be used for creating the GUI and calling the functions from main
+from main import fetchPID,verifyStatus,fetchPATH,patchBytes,injectDll
--- Source/main.py
+++ Source/main.py
@@ -1,1 +1,176 @@
 #This file will be used for creating the main functions of the injector
+import psutil
+import subprocess
+from ctypes import (WinError, byref, c_int, c_long, c_ulong,
+                    create_string_buffer, windll)
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
+import pymem
+import re
+
+#Injection method belongs to: https://github.com/numaru
+class Injector:
+    PROC_ALL_ACCESS = (0x000F0000 | 0x00100000 | 0x00000FFF)
+    MEM_CREATE = 0x00001000 | 0x00002000
+    MEM_RELEASE = 0x8000
+    PAGE_EXECUTE_READWRITE = 0x40
+
+    def __init__(self):
+        self.kernel32 = windll.kernel32
+        self.user32 = windll.user32
+        self.pid = c_ulong()
+        self.handle = None
+
+    def create_process(self, path):
+        return subprocess.Popen([path]).pid
+
+    def load_from_pid(self, pid):
+        self.unload()
+        self.pid = c_ulong(pid)
+        self.handle = self.kernel32.OpenProcess(self.PROC_ALL_ACCESS, 0, pid)
+        if not self.handle:
+            raise WinError()
+
+    def unload(self):
+        if self.handle:
+            self.kernel32.CloseHandle(self.handle)
+            if not self.handle:
+                raise WinError()
+        self.handle = None
+
+    def alloc_remote(self, buffer, size):
+        alloc = self.kernel32.VirtualAllocEx(self.handle, None, c_int(size),
+                                             self.MEM_CREATE, self.PAGE_EXECUTE_READWRITE)
+        if not alloc:
+            raise WinError()
+        self.write_memory(alloc, buffer)
+        return alloc
+
+    def free_remote(self, addr, size):
+        if not self.kernel32.VirtualFreeEx(self.handle, addr, c_int(0), self.MEM_RELEASE):
+            raise WinError()
+
+    def get_address_from_module(self, module, function):
+        module_addr = self.kernel32.GetModuleHandleA(module.encode("ascii"))
+        if not module_addr:
+            raise WinError()
+        function_addr = self.kernel32.GetProcAddress(module_addr, function.encode("ascii"))
+        if not module_addr:
+            raise WinError()
+        return function_addr
+
+    def create_remote_thread(self, function_addr, args):
+        dll_addr = c_long(0)
+        args_addr = self.alloc_remote(args, len(args))
+        thread = self.kernel32.CreateRemoteThread(self.handle, None, None, c_long(function_addr),
+                                                  c_long(args_addr), None, None)
+        if not thread:
+            raise WinError()
+        if self.kernel32.WaitForSingleObject(thread, 0xFFFFFFFF) == 0xFFFFFFFF:
+            raise WinError()
+        if not self.kernel32.GetExitCodeThread(thread, byref(dll_addr)):
+            raise WinError()
+        self.free_remote(args_addr, len(args))
+        return dll_addr.value
+
+    def read_memory(self, addr, size):
+        buffer = create_string_buffer(size)
+        if not self.kernel32.ReadProcessMemory(self.handle, c_long(addr), buffer, size, None):
+            raise WinError()
+        return buffer
+
+    def write_memory(self, addr, string):
+        size = len(string)
+        if not self.kernel32.WriteProcessMemory(self.handle, addr, string, size, None):
+            raise WinError()
+
+    def load_library(self, buffer):
+        function_addr = self.get_address_from_module("kernel32.dll", "LoadLibraryA")
+        dll_addr = self.create_remote_thread(function_addr, buffer)
+        return dll_addr
+
+    def inject_dll(self, path):
+        return self.load_library(path.encode("ascii"))
+
+    def call_from_injected(self, path, dll_addr, function, args):
+        function_offset = self.get_offset_of_exported_function(path.encode("ascii"), function)
+        self.create_remote_thread(dll_addr + function_offset, args)
+
+    def get_offset_of_exported_function(self, module, function):
+        base_addr = self.kernel32.LoadLibraryA(module)
+        if not base_addr:
+            raise WinError()
+        function_addr = self.kernel32.GetProcAddress(base_addr, function.encode("ascii"))
+        if not function_addr:
+            raise WinError()
+        if not self.kernel32.FreeLibrary(base_addr):
+            raise WinError()
+        return function_addr - base_addr
+
+#Finding the PID of csgo.exe
+
+#Initiating variables
+csgoPID = None
+csgoStatus = None
+global dllPATH
+
+def fetchPID():
+	# Iterate over all running process
+	for proc in psutil.process_iter():
+	    try:
+	            # Get process name & pid from process object.
+	        processName = proc.name()
+	        processID = proc.pid
+	        if processName == "csgo.exe":
+	            return processID
+	    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+	        pass
+
+
+
+def verifyStatus():
+	if csgoPID != None:
+	    csgoStatus = True
+
+	else:
+	    csgoStatus = False
+
+
+#Assigns the path of the dll to path_dll
+
+def fetchPATH():
+
+    global path_dll
+    path_dll = askopenfilename()
+    entry_text2.set(path_dll)
+    but2.configure(state="disabled")
+    but3.configure(state="active")
+
+def patchBytes():
+
+    pm = pymem.Pymem('csgo.exe')
+    csgo = pymem.process.module_from_name(pm.process_handle,
+                                            'csgo.exe')
+
+    csgoModule = pm.read_bytes(csgo.lpBaseOfDll, csgo.SizeOfImage)
+    address = csgo.lpBaseOfDll + re.search(rb'.\x1A\xF6\x45\x0C\x20',
+                                             csgoModule).start()
+
+    pm.write_uchar(address, 0xEB if pm.read_uchar(address) == 0x74 else 0x74)
+    pm.close_process()
+
+def injectDll():
+
+    injector = Injector()
+
+    # Enter your PID and DLL path.
+    pid = int(csgoPID)
+
+    # Load the process from a given pid.
+    injector.load_from_pid(pid)
+
+    # Inject the DLL.
+    injector.inject_dll(path_dll)
+
+    # Unload to close the process handle.
+    injector.unload()
